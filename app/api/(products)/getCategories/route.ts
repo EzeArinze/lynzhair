2eb0ae12809cac31ed218a1b59c42572d39cf775
Arinze-Eze <emmanuelrinz@gmail.com>
2025-03-06 22:56:40 +0100

    fetching products by category route done

--- app/api/(products)/getCategories/route.ts
+++ app/api/(products)/getCategories/route.ts
@@ -6,6 +6,15 @@ export async function GET() {
     await connectDB();
     const categories = await Product.distinct("category");
 
+    if (!categories) {
+      return new Response(
+        JSON.stringify({ message: "No Product Data in the DataBase" }),
+        {
+          status: 400,
+        }
+      );
+    }
+
     return new Response(
       JSON.stringify({ message: "Successfully fetched data", categories }),
       { status: 200 }
---
+++ app/api/(products)/products-categories/route.ts
@@ -0,0 +1,36 @@
+import connectDB from "@/lib/dbconnect";
+import { Product } from "@/models/ProductModel";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  try {
+    await connectDB();
+    const { searchParams } = new URL(request.url);
+    const category = searchParams.get("category");
+
+    if (!category) {
+      return new Response("Search parameter is missing", { status: 400 });
+    }
+
+    const categoryPattern = new RegExp(`^${category}$`, "i");
+    const categoryProducts = await Product.find({ category: categoryPattern });
+
+    if (!categoryProducts.length) {
+      return NextResponse.json(
+        { message: "No products found for this category", data: [] },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(
+      { message: "Successfull", data: categoryProducts },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Error fetching products:", error);
+    return NextResponse.json(
+      { message: "Something went wrong", error: (error as Error).message },
+      { status: 500 }
+    );
+  }
+}
