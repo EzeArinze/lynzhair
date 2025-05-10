760aed1f6348252eb10ee64c9e7e8c6cc8113f69
Arinze-Eze <emmanuelrinz@gmail.com>
2025-05-10 11:44:11 +0100

    reverting to resend

--- lib/nodemailer.ts
+++ lib/nodemailer.ts
@@ -1,14 +1,14 @@
-import { SMTP_PASS, SMTP_USER } from "@/lib/constant/env";
-import nodemailer from "nodemailer";
+// import { SMTP_PASS, SMTP_USER } from "@/lib/constant/env";
+// import nodemailer from "nodemailer";
 
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  auth: {
-    user: SMTP_USER,
-    pass: SMTP_PASS,
-  },
-});
+// const transporter = nodemailer.createTransport({
+//   host: "smtp.gmail.com",
+//   port: 465,
+//   secure: true,
+//   auth: {
+//     user: SMTP_USER,
+//     pass: SMTP_PASS,
+//   },
+// });
 
-export default transporter;
+// export default transporter;
